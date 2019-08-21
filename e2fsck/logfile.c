02cc2303a76200e699ddd16bbeac7a70cfaff1eb
Li Xi <lixi@ddn.com>
2019-08-21 21:58:31 +0800

    e2fsck: add start/end group for thread
    When multi-threads are used for check, each thread needs to jump
    to different group in pass1 check. This patch adds the group
    jumping support. But still, only one thread is used to check.
    
    Signed-off-by: Li Xi <lixi@ddn.com>
    Signed-off-by: Wang Shilong <wshilong@ddn.com>

--- e2fsck/logfile.c
+++ e2fsck/logfile.c
@@ -311,8 +311,9 @@ static FILE *set_up_log_file(e2fsck_t ctx, const char *key, const char *fn)
 
 	expand_logfn(ctx, log_fn, &s);
 	if (ctx->global_ctx) {
-		assert(ctx->thread_index < 1000);
-		sprintf(string_index, "%03d", ctx->thread_index);
+		assert(ctx->thread_info.et_thread_index < 1000);
+		sprintf(string_index, "%03d",
+			ctx->thread_info.et_thread_index);
 		append_string(&s, ".", 1);
 		append_string(&s, string_index, 0);
 	}
